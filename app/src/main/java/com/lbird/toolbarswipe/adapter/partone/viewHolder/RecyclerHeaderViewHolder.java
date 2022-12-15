a33e3125abb691d8713b91ca3541323ca350362b
Albert_CST <albert@coresystemtech.com>
2022-12-15 12:17:15 +0800

    Initial Commit

---
+++ app/src/main/java/com/lbird/toolbarswipe/adapter/partone/viewHolder/RecyclerHeaderViewHolder.java
@@ -0,0 +1,13 @@
+package com.lbird.toolbarswipe.adapter.partone.viewHolder;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerHeaderViewHolder extends RecyclerView.ViewHolder {
+
+    public RecyclerHeaderViewHolder(View itemView) {
+        super(itemView);
+    }
+
+}
---
+++ app/src/main/java/com/lbird/toolbarswipe/adapter/parttwo/RecyclerAdapter.java
@@ -0,0 +1,42 @@
+package com.lbird.toolbarswipe.adapter.parttwo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.lbird.toolbarswipe.R;
+import com.lbird.toolbarswipe.adapter.parttwo.viewHolder.RecyclerItemViewHolder;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<String> mItemList;
+
+    public RecyclerAdapter(List<String> itemList) {
+        mItemList = itemList;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
+        return RecyclerItemViewHolder.newInstance(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        RecyclerItemViewHolder holder = (RecyclerItemViewHolder) viewHolder;
+        String itemText = mItemList.get(position);
+        holder.setItemText(itemText);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemList == null ? 0 : mItemList.size();
+    }
+
+}
